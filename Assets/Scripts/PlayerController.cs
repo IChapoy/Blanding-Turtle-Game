76c57da5d996bc3fcccc89a31f1330b285bbdde2
IChapoy <168123695+IChapoy@users.noreply.github.com>
2024-05-09 13:09:30 -0500

    Add files via upload

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,53 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+
+    public float speed = 25;
     private Rigidbody2D rb2d;
     public float verticalInput;
-    private float yRange = 4.5f;
-    public GameObject obstaclePrefab;
-    public float horizontalInput;
-    public float xRange = 8.825f;
+    private float duration = 3;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
+    void OnTriggerEnter2D(Collider2D coli)
+    {
+        if (coli.CompareTag("Boost"))
+        {
+            speed = speed * 2;
+            Invoke(nameof(ResetEffect), duration);
+        }
+    }
+    private void ResetEffect()
+    {
+        speed = 25;
 
+        
+    }
     // Update is called once per frame
     void Update()
     {
-        Time.timeScale = 1.0f;
         verticalInput = Input.GetAxis("Vertical");
-        horizontalInput = Input.GetAxis("Horizontal");
-
         transform.Translate(Vector2.up * verticalInput * Time.deltaTime * speed);
-
-        if (horizontalInput > 0)
+        if (transform.position.y < -4.5)
         {
-            transform.Translate(Vector2.right * horizontalInput * Time.deltaTime * speed/2);
+            transform.position = new Vector2(transform.position.x, -4.5f);
         }
-        if (horizontalInput < 0)
+        if (transform.position.y > 4.5)
         {
-            transform.Translate(Vector2.right * horizontalInput * Time.deltaTime * speed*1.25f);
-        }
-
-        if (transform.position.y < -yRange)
-        {
-            transform.position = new Vector2(transform.position.x, -yRange);
-        }
-        if (transform.position.y > yRange)
-        {
-            transform.position = new Vector2(transform.position.x, yRange);
-        }
-        if (transform.position.x > -xRange)
-        {
-            transform.position = new Vector2(-xRange, transform.position.y);
-        }
-        if (transform.position.x < xRange)
-        {
-           transform.position = new Vector2(xRange, transform.position.y);
+            transform.position = new Vector2(transform.position.x, 4.5f);
         }
     }
     
