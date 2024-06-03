c9853ab3082d7892f637ccd158b298871772fc96
IChapoy <168123695+IChapoy@users.noreply.github.com>
2024-06-02 22:45:40 -0500

    beginning of importing sound + new sprites
    added crunch sound effects into scripts, imported new pngs

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -7,12 +7,15 @@ public class Score : MonoBehaviour
 {
     public int score;
     public int pointsPerCollect = 25;
-   
+    public AudioSource crunch1;
+    public AudioSource crunch2;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        crunch1 = this.gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,6 +30,14 @@ public class Score : MonoBehaviour
             score += pointsPerCollect;
             Destroy(other.gameObject);
             UpdateScoreDisplay();
+            if (other.gameObject.name == "snail shell_0(Clone)" )
+            {
+                crunch2.Play();
+            } else
+            {
+                crunch1.Play();
+
+            }
         }
         else if (other.CompareTag("Obstacle")) {
             score -= pointsPerCollect;
